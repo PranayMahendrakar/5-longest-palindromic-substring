eb575a19cab04476a2d9af0b2c80e224871f3390
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-06 10:39:13 +0530

    Initial commit

---
+++ 2025-12-23 16.37.35 - Accepted - runtime 362ms - memory 12.7MB.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        if not s:
+            return ""
+        
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+        
+        result = ""
+        for i in range(len(s)):
+            # Odd length palindrome
+            odd = expand(i, i)
+            if len(odd) > len(result):
+                result = odd
+            # Even length palindrome
+            even = expand(i, i + 1)
+            if len(even) > len(result):
+                result = even
+        
+        return result
